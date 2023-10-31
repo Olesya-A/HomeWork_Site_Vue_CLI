ac4210d767450434a9fdea57ac261190e0ddc49c
Olesya <lesusha1@yandex.ru>
2023-10-31 18:13:49 +0300

    added main component

---
+++ src/components/Main.vue
@@ -0,0 +1,375 @@
+<template>
+  <header class="header center">
+    <a class="header__left" href="main.html">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="34"
+        height="34"
+        viewBox="0 0 34 34"
+        fill="none"
+      >
+        <path
+          d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
+          fill="#CDA274"
+        />
+      </svg>
+      <h3 class="header__left_title">Interno</h3>
+    </a>
+    <nav class="header__right">
+      <a href="#" class="header__right_nav">Home</a>
+      <a href="#" class="header__right_nav">Project</a>
+      <a href="#" class="header__right_nav">Blog</a>
+    </nav>
+  </header>
+  <div class="main_banner center">
+    <div class="main_banner__cover">
+      <h1 class="main_banner__cover_header">Let Your Home Be Unique</h1>
+      <p class="main_banner__cover_text">
+        There are many variations of the passages of lorem Ipsum fromavailable,
+        majority.
+      </p>
+      <div class="main_banner__cover_button">
+        <a class="">Get Started</a>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="17"
+          viewBox="0 0 18 17"
+          fill="none"
+        >
+          <path
+            d="M1 8.43542L14.7232 8.29857M9.61818 1.91138L16.1412 8.43436L9.48677 15.0887"
+            stroke="#CDA274"
+            stroke-width="2"
+            stroke-linecap="square"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
+    </div>
+  </div>
+  <section class="project center">
+    <div class="project__heading">
+      <h2 class="project__heading_header">Follow Our Projects</h2>
+      <p class="project__heading_text">
+        It is a long established fact that a reader will be distracted by the of
+        readable content of page lookings at its layouts points.
+      </p>
+    </div>
+    <div class="project__catalog">
+      <article class="project__catalog_item">
+        <div class="project__catalog_item_img img_1">
+          <img
+            class="project__catalog_item_img_1"
+            src="../assets/image/project_item_1.jpg"
+            alt="picture"
+          />
+        </div>
+        <div class="project__catalog_item_content">
+          <div class="txtcontent">
+            <h3 class="txtcontent_header">Modern Kitchan</h3>
+            <p class="txtcontent_text">Decor / Artchitecture</p>
+          </div>
+          <div class="arrow">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="70"
+              height="70"
+              viewBox="0 0 70 70"
+              fill="none"
+            >
+              <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
+              <path
+                d="M32 44L40 35L32 26"
+                stroke="#292F36"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+      </article>
+      <article class="project__catalog_item">
+        <div class="project__catalog_item_img img_2">
+          <img
+            class="project__catalog_item_img_2"
+            src="../assets/image/project_item_2.jpg"
+            alt="picture"
+          />
+        </div>
+        <div class="project__catalog_item_content">
+          <div class="txtcontent">
+            <h3 class="txtcontent_header">Modern Kitchan</h3>
+            <p class="txtcontent_text">Decor / Artchitecture</p>
+          </div>
+          <div class="arrow">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="70"
+              height="70"
+              viewBox="0 0 70 70"
+              fill="none"
+            >
+              <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
+              <path
+                d="M32 44L40 35L32 26"
+                stroke="#292F36"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+      </article>
+      <article class="project__catalog_item">
+        <div class="project__catalog_item_img img_3">
+          <img
+            class="project__catalog_item_img_3"
+            src="../assets/image/project_item_3.jpg"
+            alt="picture"
+          />
+        </div>
+        <div class="project__catalog_item_content">
+          <div class="txtcontent">
+            <h3 class="txtcontent_header">Modern Kitchan</h3>
+            <p class="txtcontent_text">Decor / Artchitecture</p>
+          </div>
+          <div class="arrow">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="70"
+              height="70"
+              viewBox="0 0 70 70"
+              fill="none"
+            >
+              <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
+              <path
+                d="M32 44L40 35L32 26"
+                stroke="#292F36"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+      </article>
+      <article class="project__catalog_item">
+        <div class="project__catalog_item_img img_4">
+          <img
+            class="project__catalog_item_img_4"
+            src="../assets/image/project_item_4.jpg"
+            alt="picture"
+          />
+        </div>
+        <div class="project__catalog_item_content">
+          <div class="txtcontent">
+            <h3 class="txtcontent_header">Modern Kitchan</h3>
+            <p class="txtcontent_text">Decor / Artchitecture</p>
+          </div>
+          <div class="arrow">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="70"
+              height="70"
+              viewBox="0 0 70 70"
+              fill="none"
+            >
+              <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
+              <path
+                d="M32 44L40 35L32 26"
+                stroke="#292F36"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+      </article>
+    </div>
+  </section>
+  <div class="counter">
+    <div class="counter__item item_border">
+      <h1 class="counter__item_header">12</h1>
+      <p class="counter__item_text">Years Of Experiance</p>
+    </div>
+    <div class="counter__item item_border">
+      <h1 class="counter__item_header">85</h1>
+      <p class="counter__item_text">Success Project</p>
+    </div>
+    <div class="counter__item item_border">
+      <h1 class="counter__item_header">15</h1>
+      <p class="counter__item_text">Active Project</p>
+    </div>
+    <div class="counter__item">
+      <h1 class="counter__item_header">95</h1>
+      <p class="counter__item_text">Happy CUstomers</p>
+    </div>
+  </div>
+  <section class="blog center">
+    <div class="blog__heading">
+      <h2 class="blog__heading_header">Articles & News</h2>
+      <p class="blog__heading_text">
+        It is a long established fact that a reader will be distracted by the of
+        readable content of a page when lookings at its layouts the points of
+        using.
+      </p>
+    </div>
+    <div class="blog__content">
+      <article class="blog__content_item">
+        <div class="blog__content_item_img">
+          <img src="../assets/image/blog_item_1.jpg" alt="image" />
+          <div class="blog__content_item_label">
+            <p class="blog__content_item_label_text">Kitchan Design</p>
+          </div>
+        </div>
+        <div class="blog__content_item_txtcontent">
+          <h3 class="txtcontent_header">
+            Let’s Get Solution For Building Construction Work
+          </h3>
+          <div class="wrapper">
+            <p class="txtcontent_text">26 December,2022</p>
+            <svg
+              class="arrow"
+              xmlns="http://www.w3.org/2000/svg"
+              width="52"
+              height="53"
+              viewBox="0 0 52 53"
+              fill="none"
+            >
+              <circle cx="26" cy="26.267" r="26" fill="#F4F0EC" />
+              <path
+                d="M23.7714 32.9527L29.7143 26.267L23.7714 19.5813"
+                stroke="#292F36"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+      </article>
+      <article class="blog__content_item">
+        <div class="blog__content_item_img">
+          <img src="../assets/image/blog_item_2.jpg" alt="image" />
+          <div class="blog__content_item_label">
+            <p class="blog__content_item_label_text">Living Design</p>
+          </div>
+        </div>
+        <div class="blog__content_item_txtcontent">
+          <h3 class="txtcontent_header">
+            Low Cost Latest Invented Interior Designing Ideas.
+          </h3>
+          <div class="wrapper">
+            <p class="txtcontent_text">26 December,2022</p>
+            <svg
+              class="arrow"
+              xmlns="http://www.w3.org/2000/svg"
+              width="52"
+              height="53"
+              viewBox="0 0 52 53"
+              fill="none"
+            >
+              <circle cx="26" cy="26.267" r="26" fill="white" />
+              <path
+                d="M23.7714 32.9527L29.7143 26.267L23.7714 19.5813"
+                stroke="#292F36"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+      </article>
+      <article class="blog__content_item">
+        <div class="blog__content_item_img">
+          <img src="../assets/image/blog_item_3.jpg" alt="image" />
+          <div class="blog__content_item_label">
+            <p class="blog__content_item_label_text">Interior Design</p>
+          </div>
+        </div>
+        <div class="blog__content_item_txtcontent">
+          <h3 class="txtcontent_header">
+            Best For Any Office & Business Interior Solution
+          </h3>
+          <div class="wrapper">
+            <p class="txtcontent_text">26 December,2022</p>
+            <svg
+              class="arrow"
+              xmlns="http://www.w3.org/2000/svg"
+              width="52"
+              height="53"
+              viewBox="0 0 52 53"
+              fill="none"
+            >
+              <circle cx="26" cy="26.267" r="26" fill="#F4F0EC" />
+              <path
+                d="M23.7714 32.9527L29.7143 26.267L23.7714 19.5813"
+                stroke="#292F36"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+      </article>
+    </div>
+  </section>
+  <footer class="footer center">
+    <div class="footer__link">
+      <a class="footer__link_logo" href="main.html">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="34"
+          height="34"
+          viewBox="0 0 34 34"
+          fill="none"
+        >
+          <path
+            d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
+            fill="#CDA274"
+          />
+        </svg>
+        <h3 class="footer__link_logo_title">Interno</h3>
+      </a>
+      <p class="footer__link_text">
+        It is a long established fact that a reader will be distracted lookings.
+      </p>
+      <div class="footer__link_icons">
+        <a href="#"
+          ><img src="../assets/image/footer_icon_1.svg" alt="twitter"
+        /></a>
+        <a href="#"
+          ><img src="../assets/image/footer_icon_2.svg" alt="insta"
+        /></a>
+      </div>
+    </div>
+    <div class="footer__pages">
+      <h3 class="footer__pages_header">Pages</h3>
+      <a class="footer__pages_item">Home</a>
+      <a class="footer__pages_item">Project</a>
+      <a class="footer__pages_item">Blog</a>
+    </div>
+    <div class="footer__contacts">
+      <h3 class="footer__contacts_header">Contact</h3>
+      <div class="footer__contacts_wrapper">
+        <p class="footer__contacts_text">
+          55 East Birchwood Ave. Brooklyn, New York 11201
+        </p>
+        <p class="footer__contacts_text">contact@interno.com</p>
+        <p class="footer__contacts_text">(123) 456 - 7890</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss"></style>
