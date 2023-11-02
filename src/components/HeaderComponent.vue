5ee3278fe6839ce3617e230e6b77dc08b435634b
Olesya <lesusha1@yandex.ru>
2023-11-02 13:26:09 +0300

    added header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header center">
+    <a class="header__left" href="main.html">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="34"
+        height="34"
+        viewBox="0 0 34 34"
+        fill="none"
+      >
+        <path
+          d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
+          fill="#CDA274"
+        />
+      </svg>
+      <h3 class="header__left_title">Interno</h3>
+    </a>
+    <nav class="header__right">
+      <a href="#" class="header__right_nav">Home</a>
+      <a href="#" class="header__right_nav">Project</a>
+      <a href="#" class="header__right_nav">Blog</a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
