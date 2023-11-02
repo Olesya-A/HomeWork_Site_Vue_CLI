dc48002562e63dee1055f23e515ade65f4bc1842
Olesya <lesusha1@yandex.ru>
2023-11-02 13:25:14 +0300

    modified main, blog and blogdetails components

--- src/components/BlogDetails.vue
+++ src/components/BlogDetails.vue
@@ -1,26 +1,5 @@
 <template>
-  <header class="header center">
-    <a class="header__left" href="main.html">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="34"
-        height="34"
-        viewBox="0 0 34 34"
-        fill="none"
-      >
-        <path
-          d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
-          fill="#CDA274"
-        />
-      </svg>
-      <h3 class="header__left_title">Interno</h3>
-    </a>
-    <nav class="header__right">
-      <a href="#" class="header__right_nav">Home</a>
-      <a href="#" class="header__right_nav">Project</a>
-      <a href="#" class="header__right_nav">Blog</a>
-    </nav>
-  </header>
+  <HeaderComponent />
   <div class="details_banner center"></div>
   <div class="articles center">
     <div class="articles__details">
@@ -76,55 +55,13 @@
       </div>
     </div>
   </div>
-  <footer class="footer center">
-    <div class="footer__link">
-      <a class="footer__link_logo" href="main.html">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="34"
-          height="34"
-          viewBox="0 0 34 34"
-          fill="none"
-        >
-          <path
-            d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
-            fill="#CDA274"
-          />
-        </svg>
-        <h3 class="footer__link_logo_title">Interno</h3>
-      </a>
-      <p class="footer__link_text">
-        It is a long established fact that a reader will be distracted lookings.
-      </p>
-      <div class="footer__link_icons">
-        <a href="#"
-          ><img src="../assets/image/footer_icon_1.svg" alt="twitter"
-        /></a>
-        <a href="#"
-          ><img src="../assets/image/footer_icon_2.svg" alt="insta"
-        /></a>
-      </div>
-    </div>
-    <div class="footer__pages">
-      <h3 class="footer__pages_header">Pages</h3>
-      <a class="footer__pages_item">Home</a>
-      <a class="footer__pages_item">Project</a>
-      <a class="footer__pages_item">Blog</a>
-    </div>
-    <div class="footer__contacts">
-      <h3 class="footer__contacts_header">Contact</h3>
-      <div class="footer__contacts_wrapper">
-        <p class="footer__contacts_text">
-          55 East Birchwood Ave. Brooklyn, New York 11201
-        </p>
-        <p class="footer__contacts_text">contact@interno.com</p>
-        <p class="footer__contacts_text">(123) 456 - 7890</p>
-      </div>
-    </div>
-  </footer>
+  <FooterComponent />
 </template>
 
 <script>
+import FooterComponent from "./FooterComponent.vue";
+import HeaderComponent from "./HeaderComponent.vue";
+
 export default {
   data() {
     return {
@@ -306,7 +243,6 @@ export default {
           tagName: "Bedroom",
         },
       ],
-
       temp: [
         {
           id: 1,
@@ -341,6 +277,7 @@ export default {
       this.temp = this.details.filter((el) => el.id === id);
     },
   },
+  components: { HeaderComponent, FooterComponent },
 };
 </script>
 
