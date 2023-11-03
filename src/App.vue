547cabf1eddbac6914df7102c1fd7a5e4b18879d
Olesya <lesusha1@yandex.ru>
2023-11-03 14:23:24 +0300

    modified app

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
   <Main />
   <Blog />
   <BlogDetails />
+  <ProjectComponent />
 </template>
 
 <script>
 import Main from "./components/Main.vue";
 import Blog from "./components/Blog.vue";
 import BlogDetails from "./components/BlogDetails.vue";
+import ProjectComponent from "./components/ProjectComponent.vue";
 
 export default {
-  components: { Main, Blog, BlogDetails },
+  components: { Main, Blog, BlogDetails, ProjectComponent },
 };
 </script>
 
